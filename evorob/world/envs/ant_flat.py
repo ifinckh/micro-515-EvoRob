0909b54880082689f825d0ea0a819678324f8020
Mathis Finckh <mathis.finckh@epfl.ch>
2026-03-19 12:09:06 +0100

    Add previous code

--- evorob/world/envs/ant_flat.py
+++ evorob/world/envs/ant_flat.py
@@ -104,21 +104,68 @@ class AntFlatEnvironment(MujocoEnv):
         # - velocity: self.data.qvel.flatten() (14 values)
         # This gives 27 total dimensions, making the task translation-invariant
         # Hint: Use np.concatenate() to combine both arrays
+        pos = self.data.qpos[2:].flatten() # 13 values
+        vel = self.data.qvel.flatten() # 14 values
+        obs = np.concatenate((pos,vel))
+        return obs
+    
         raise NotImplementedError("TODO: Implement observation function")
 
     def _get_rew(self, x_velocity: float, action):
         # TODO: Implement reward function with three components:
-        # 1. forward_reward = x_velocity * forward_reward_weight (weight=1.0)
-        # 2. healthy_reward = healthy_reward_weight (weight=1.0)
-        # 3. ctrl_cost = ctrl_cost_weight * sum of squared actions (weight=0.5)
-        # Final reward = forward_reward + healthy_reward - ctrl_cost
+        # 1. forward_reward = ...
+        # 2. healthy_reward = ...
+        # 3. ctrl_cost = ...
+        # Final reward is the sum of these three components.
+        # 1. forward_reward = ...
+        # 2. healthy_reward = ...
+        # 3. ctrl_cost = ...
+        # Final reward is the sum of these three components.
         # Return: (reward, reward_info_dict)
+        
+        # reward not terminating
+        healthy_reward = not self._get_termination()
+        # reward forward velocity
+        forward_reward = x_velocity #* healthy_reward
+        # reward low motor toques (penalize high control inputs)
+        ctrl_cost = np.sum(np.square(action))
+        # reward distance traveled in the forward (x) direction
+        distance_reward = x_velocity * self.dt
+        # penalty for standing still, track last 5 positions and penalize if not moving 
+                
+        
+        # recommended weights
+        # weights = np.array([1,1,-0.5, 0])
+        weights = np.array([7,0.5,-0.5,3])
+        reward_array = np.array([forward_reward, healthy_reward, ctrl_cost, distance_reward])
+        
+        reward = np.sum(reward_array * weights)
+        
+        # have access to the individual losses just in case
+        reward_info_dict = {
+            "reward_forward" : forward_reward,
+            "reward_survive" : healthy_reward, 
+            "reward_ctrl" : ctrl_cost}
+        
+        # print(reward_info_dict)
+        
+        return (reward, reward_info_dict)
+    
         raise NotImplementedError("TODO: Implement reward function")
 
     def _get_termination(self):
         # TODO: Robot should terminate when:
-        # - Any value in state is not finite (check with np.isfinite(state).all())
-        # - Torso height (state[2]) is below 0.26 or above 1.0
+        # - Torso height is below 0.26 or above 1.0
         # Return True if NOT healthy (i.e., should terminate)
-        # Hint: Use self.state_vector() to get current state
+        # Hint: Use self.state_vector() to get current state.
+        
+        torso_height = self.state_vector()[2]
+        
+        # termination condition
+        if (torso_height < 0.26) or (torso_height > 1.0):
+            return True
+        else:
+            return False
+        
+        
         raise NotImplementedError("TODO: Implement termination function")
--- evorob/world/robot/controllers/mlp.py
+++ evorob/world/robot/controllers/mlp.py
@@ -30,14 +30,12 @@ class NeuralNetworkController(Controller):
         # - self.input_to_hidden: shape (hidden_size, input_size)
         # - self.hidden_to_output: shape (output_size, hidden_size)
         # Hint: Use np.random.uniform(-1, 1, (rows, cols))
-        self.input_to_hidden = ...  # TODO!
-        self.hidden_to_output = ...  # TODO!
+        self.input_to_hidden = np.random.uniform(-1, 1, (hidden_size, input_size))  # TODO!
+        self.hidden_to_output = np.random.uniform(-1, 1, (output_size, hidden_size))  # TODO!
 
         # TODO: Compute number of parameters in each layer
-        # - self.n_params_i2h = input_size * hidden_size
-        # - self.n_params_h2o = hidden_size * output_size
-        self.n_params_i2h = ...  # TODO!
-        self.n_params_h2o = ...  # TODO!
+        self.n_params_i2h = hidden_size * input_size  # TODO!
+        self.n_params_h2o = output_size * hidden_size  # TODO!
 
         self.n_params = self.get_num_params()
 
@@ -52,13 +50,34 @@ class NeuralNetworkController(Controller):
             action: Output array, shape (output_size,) or (batch_size, output_size)
         """
         # TODO: Perform forward pass computation
-        # 1. Hidden layer: hidden = tanh(state @ input_to_hidden.T)
-        # 2. Output layer: output = tanh(hidden @ hidden_to_output.T)
+        # 1. Hidden layer: hidden = ...
+        # 2. Output layer: output = ...
+        # 1. Hidden layer: hidden = ...
+        # 2. Output layer: output = ...
         # 3. Clip output to [-1, 1] using np.clip()
         #
         # Hint: Use @ operator or np.matmul for matrix multiplication
         # Hint: .T transposes a matrix
         # Hint: np.tanh() applies tanh element-wise
+        
+        # if state of shape (input_size,) 
+        if state.shape[0] == self.n_input:
+            hidden = np.tanh(self.input_to_hidden @ state) # (hidden_size, input_size) x (input_size,) = (hidden_size, )
+            output = np.tanh(self.hidden_to_output @ hidden) # (output_size, hidden_size) x (hidden_size, ) = (output_size, )
+        # if state of shape (batch_size, input_size)
+        else:
+            hidden = np.tanh(self.input_to_hidden @ state.T) # (hidden_size, input_size) x (input_size, batch_size) = (hidden_size, batch_size)
+            output = np.tanh(self.hidden_to_output @ hidden) # (output_size, hidden_size) x (hidden_size, batch_size) = (output_size, batch_size)
+            
+        # hidden = np.tanh(np.matmul(self.input_to_hidden, state.T)) # (hidden_size, input_size) x (input_size, batch_size) = (hidden_size, batch_size)
+        # output = np.tanh(np.matmul(self.hidden_to_output,hidden)) # (output_size, hidden_size) x (hidden_size, batch_size) = (output_size, batch_size)
+        
+        # print(output)
+        
+            
+        # clip just in case, but tanh should do that either way
+        return np.clip(output.T, a_min = -1, a_max = 1)
+    
         raise NotImplementedError("TODO: Implement forward pass")
 
     def set_weights(self, encoding):
@@ -75,6 +94,12 @@ class NeuralNetworkController(Controller):
         #
         # Hint: Use array slicing: encoding[:n] and encoding[n:]
         # Hint: Use np.reshape(array, (rows, cols)) or array.reshape((rows, cols))
+        
+        self.input_to_hidden =  encoding[:self.n_hidden*self.n_input].reshape((self.n_hidden, self.n_input))
+        self.hidden_to_output =  encoding[self.n_hidden*self.n_input:].reshape((self.n_output, self.n_hidden))
+        
+        return
+
         raise NotImplementedError("TODO: Implement weight setting")
 
     def geno2pheno(self, genotype):
@@ -84,7 +109,14 @@ class NeuralNetworkController(Controller):
     def get_num_params(self):
         # To provide a genetic encoding for our neural network controller,
         # we compute and store the number of parameters in our NN class.
-        # TODO: Return sum of parameters in both layers!
+        # TODO: Return the total number of parameters in both layers!
+        
+        i2h = self.n_input*self.n_hidden
+        h2o = self.n_output*self.n_hidden
+        
+        return i2h + h2o
+    
+    
         raise NotImplementedError
 
     def reset_controller(self, batch_size=1) -> None:
